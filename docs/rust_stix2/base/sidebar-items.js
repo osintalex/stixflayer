31471421e650335d9cc48562f1dcee009fca199a
osintalex <63904626+osintalex@users.noreply.github.com>
2026-04-19 18:10:57 +0100

    refactor: move to new polyglot repo layout

--- docs/rust_stix2/base/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"enum":["BuilderType","StixObject"],"struct":["CommonProperties","CommonPropertiesBuilder"],"trait":["Stix"]};
--- stix/src/domain_objects/mod.rs
+++
@@ -1,6 +0,0 @@
-//! Data structures and functions for implementing STIX Domain Objects (SDOs).
-
-pub mod sdo;
-pub mod sdo_types;
-mod tests;
-pub mod vocab;
